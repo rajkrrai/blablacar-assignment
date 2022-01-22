2cdd701145609a9a5cf706cb585b979fc11300db
rajkrrai <64032301+rajkrrai@users.noreply.github.com>
2022-01-23 00:56:03 +0100

    deployment try

--- src/services/RideFetchingService.js
+++ src/services/RideFetchingService.js
@@ -12,7 +12,7 @@ let FetchRideDetails = async () => {
   //     `https://public-api.blablacar.com/api/v3/trips?key=${process.env.REACT_APP_APIKEY}&from_coordinate=48.8566%2C2.3522&to_coordinate=45.764043%2C4.835659&from_country=FR&to_country=FR&locale=en-GB&currency=EUR&start_date_local=${apiDate}&count=100`
   //   );
   return axios.get(
-    `https://public-api.blablacar.com/api/v3/trips?key=${env.REACT_APP_APIKEY}&from_coordinate=48.8566%2C2.3522&to_coordinate=45.764043%2C4.835659&from_country=FR&to_country=FR&locale=en-GB&currency=EUR&start_date_local=2022-01-23T12:00:00&count=100`
+    `https://public-api.blablacar.com/api/v3/trips?key=YzbiA8L6DcqxTvSna1lOFQQU66FosDVs&from_coordinate=48.8566%2C2.3522&to_coordinate=45.764043%2C4.835659&from_country=FR&to_country=FR&locale=en-GB&currency=EUR&start_date_local=2022-01-23T12:00:00&count=100`
   );
 };
 
