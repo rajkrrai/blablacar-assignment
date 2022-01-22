15378b10a2fdf400fda3180ea7d131f4a4b3bae4
rajkrrai <64032301+rajkrrai@users.noreply.github.com>
2022-01-22 18:02:58 +0100

    key issue part 1

--- src/services/RideFetchingService.js
+++ src/services/RideFetchingService.js
@@ -6,7 +6,7 @@ today = moment(today).format("D, MMMM");
 let apiDate = moment.parseZone(today).local(true).format().split("+")[0]; //2022-01-21T12:00:00
 
 let FetchRideDetails = async () => {
-  console.log(process.env);
+  console.log(process.env.NODE_ENV);
   return axios.get(
     `https://public-api.blablacar.com/api/v3/trips?key=${process.env.REACT_APP_APIKEY}&from_coordinate=48.8566%2C2.3522&to_coordinate=45.764043%2C4.835659&from_country=FR&to_country=FR&locale=en-GB&currency=EUR&start_date_local=${apiDate}&count=100`
   );
